bb2372a2250532d3916737d1c2e85306f94f25c8
bintangadinandra <bintangadinandra@gmail.com>
2020-08-12 15:54:23 +0700

    fix: update appendBFromA fn

--- packages/package-b/index.js
+++ packages/package-b/index.js
@@ -2,7 +2,7 @@ const quoteMeFromA = require('package-a');
 
 function appendBFromA() {
     const a = quoteMeFromA();
-    return a + ' From B'
+    return a + ' From B, Whoops! another update'
 }
 
 module.exports = appendBFromA
