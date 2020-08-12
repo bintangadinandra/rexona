a1a98703a3c59dafeeac1b2d623036ebd8c51d90
bintangadinandra <bintangadinandra@gmail.com>
2020-08-12 15:17:50 +0700

    feat: add new package c

--- packages/package-b/index.js
+++ packages/package-b/index.js
@@ -1,3 +1,8 @@
 const quoteMeFromA = require('package-a');
 
-console.log(quoteMeFromA())
+function appendBFromA() {
+    const a = quoteMeFromA();
+    return a + ' From B'
+}
+
+module.exports = appendBFromA
---
+++ packages/package-c/index.js
@@ -0,0 +1,5 @@
+const packA = require('package-a');
+const packB = require('package-b');
+
+console.log(packA())
+console.log(packB())
